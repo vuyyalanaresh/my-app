b55ca91666a66963ef9f24dca4e167c49530552c
vuyyalanaresh <vnareshgoud24@gmail.com>
2024-11-27 10:47:34 +0530

    loginviewcomplete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+public LoginForm:FormGroup = new FormGroup({
+  email:new FormControl(),
+  password:new FormControl()
 
+})
+constructor(private _LoginService:LoginService){}
+login(){
+console.log(this.LoginForm);
+this._LoginService.login(this.LoginForm.value).subscribe(
+  (data:any)=>{
+    alert("Login Sucess");
+  },
+  (err:any)=>{
+    alert("invalid");
+  }
+)
+}
 }
